ed4fdccdde638bc924e6e00af8eddb9cc8c29f62
KodrAus <kodraus@hey.com>
2021-09-15 13:26:52 +1000

    start figuring out validity

--- src/stream.rs
+++ src/stream.rs
@@ -6,41 +6,86 @@ use crate::{erased, value};
 pub use crate::{for_all::ForAll, Error, Result};
 
 pub trait Stream<'a> {
-    fn display<D: fmt::Display>(&mut self, fmt: D) -> Result;
+    // This high-level API describes the valid ways the low-level flat API
+    // can be used: whether or not pairs must contain values
 
-    fn u64(&mut self, value: u64) -> Result;
-    fn i64(&mut self, value: i64) -> Result;
-    fn u128(&mut self, value: u128) -> Result;
-    fn i128(&mut self, value: i128) -> Result;
-    fn f64(&mut self, value: f64) -> Result;
-    fn bool(&mut self, value: bool) -> Result;
-    fn none(&mut self) -> Result;
+    fn any<'a, V: ValueRef<'a>>(&mut self, value: V) -> Result {
+        value.stream(self)
+    }
 
-    fn str<'s, S: TypedRef<'s, str>>(&mut self, value: S) -> Result
-    where
-        's: 'a;
-
-    fn error<'e, E: TypedRef<'e, dyn error::Error + 'static>>(&mut self, error: E) -> Result
-    where
-        'e: 'a;
-
-    fn type_tag<T: TypedRef<'static, str>>(&mut self, tag_type: T) -> Result;
-    fn value_tag<T: TypedRef<'static, str>, K: TypedRef<'static, str>>(
-        tag_type: T,
-        tag_key: V,
-        tag_index: Option<u64>,
-    ) -> Result;
-
-    fn type_tagged_begin<T: TypedRef<'static, str>>(&mut self, tag_type: T) -> Result;
-    fn type_tagged_end(&mut self) -> Result;
-
-    fn value_tagged_begin<T: TypedRef<'static, str>, K: TypedRef<'static, str>>(
+    fn map<'k, 'v, I: IntoIterator<Item = (K, V)>, K: ValueRef<'k>, V: ValueRef<'v>>(
         &mut self,
-        tag_type: T,
-        tag_key: K,
-        tag_index: Option<u64>,
-    ) -> Result;
-    fn value_tagged_end(&mut self) -> Result;
+        map: I,
+    ) -> Result
+    where
+        'k: 'a,
+        'v: 'a,
+    {
+        let mut map = map.into_iter();
+
+        self.map_begin(map.size_hint().1)?;
+        for (k, v) in map {
+            self.map_entry(k, v)?;
+        }
+        self.map_end()
+    }
+
+    fn map_field<'v, F: TypedRef<'static, str>, V: ValueRef<'v>>(
+        &mut self,
+        field: F,
+        value: V,
+    ) -> Result
+    where
+        'v: 'a,
+    {
+        self.map_entry(field, value)
+    }
+
+    fn map_entry<'k, 'v, K: ValueRef<'k>, V: ValueRef<'v>>(&mut self, key: K, value: V) -> Result
+    where
+        'k: 'a,
+        'v: 'a,
+    {
+        self.map_key(key)?;
+        self.map_value(value)
+    }
+
+    fn map_key<'k, K: ValueRef<'k>>(&mut self, key: K) -> Result
+    where
+        'k: 'a,
+    {
+        self.map_key_begin()?;
+        key.stream(self)?;
+        self.map_key_end()
+    }
+
+    fn map_value<'v, V: ValueRef<'v>>(&mut self, value: V) -> Result
+    where
+        'v: 'a,
+    {
+        self.map_value_begin()?;
+        value.stream(self)?;
+        self.map_value_end()
+    }
+
+    fn seq<'e, I: IntoIterator<Item = E>, E: ValueRef<'e>>(&mut self, seq: I) -> Result {
+        let seq = seq.into_iter();
+
+        self.seq_begin(seq.size_hint().1)?;
+        for elem in seq {
+            self.seq_elem(elem)?;
+        }
+        self.seq_end()
+    }
+
+    fn seq_elem<'e, E: ValueRef<'e>>(&mut self, elem: E) -> Result
+    where
+        'e: 'a,
+    {
+        self.seq_elem_begin()?;
+        elem.stream(self)?;
+        self.seq_elem_end()
+    }
 
     fn type_tagged<'v, T: TypedRef<'static, str>, V: ValueRef<'v>>(
         &mut self,
@@ -70,67 +115,63 @@ pub trait Stream<'a> {
         self.value_tagged_end()
     }
 
+    fn display<D: fmt::Display>(&mut self, fmt: D) -> Result;
+
+    fn u64(&mut self, value: u64) -> Result;
+    fn i64(&mut self, value: i64) -> Result;
+    fn u128(&mut self, value: u128) -> Result;
+    fn i128(&mut self, value: i128) -> Result;
+    fn f64(&mut self, value: f64) -> Result;
+    fn bool(&mut self, value: bool) -> Result;
+    fn none(&mut self) -> Result;
+
+    fn str<'s, S: TypedRef<'s, str>>(&mut self, value: S) -> Result
+    where
+        's: 'a;
+
+    fn error<'e, E: TypedRef<'e, dyn error::Error + 'static>>(&mut self, error: E) -> Result
+    where
+        'e: 'a;
+
+    fn type_tag<T: TypedRef<'static, str>>(&mut self, tag_type: T) -> Result;
+    fn value_tag<T: TypedRef<'static, str>, K: TypedRef<'static, str>>(
+        tag_type: T,
+        tag_key: V,
+        tag_index: Option<u64>,
+    ) -> Result;
+
+    // Pair *must* be contained by a single value
+    fn type_tagged_begin<T: TypedRef<'static, str>>(&mut self, tag_type: T) -> Result;
+    fn type_tagged_end(&mut self) -> Result;
+
+    // Pair *must* be contained by a single value
+    fn value_tagged_begin<T: TypedRef<'static, str>, K: TypedRef<'static, str>>(
+        &mut self,
+        tag_type: T,
+        tag_key: K,
+        tag_index: Option<u64>,
+    ) -> Result;
+    fn value_tagged_end(&mut self) -> Result;
+
+    // Pair may be empty
     fn map_begin(&mut self, len: Option<usize>) -> Result;
     fn map_end(&mut self) -> Result;
 
+    // Pair *must* be contained by a single value
     fn map_key_begin(&mut self) -> Result;
     fn map_key_end(&mut self) -> Result;
 
+    // Pair *must* be contained by a single value
     fn map_value_begin(&mut self) -> Result;
     fn map_value_end(&mut self) -> Result;
 
-    fn map_key<'k, K: ValueRef<'k>>(&mut self, key: K) -> Result
-    where
-        'k: 'a,
-    {
-        self.map_key_begin()?;
-        key.stream(self)?;
-        self.map_key_end()
-    }
-
-    fn map_value<'v, V: ValueRef<'v>>(&mut self, value: V) -> Result
-    where
-        'v: 'a,
-    {
-        self.map_value_begin()?;
-        value.stream(self)?;
-        self.map_value_end()
-    }
-
-    fn map_entry<'k, 'v, K: ValueRef<'k>, V: ValueRef<'v>>(&mut self, key: K, value: V) -> Result
-    where
-        'k: 'a,
-        'v: 'a,
-    {
-        self.map_key(key)?;
-        self.map_value(value)
-    }
-
-    fn map_field<'v, F: TypedRef<'static, str>, V: ValueRef<'v>>(
-        &mut self,
-        field: F,
-        value: V,
-    ) -> Result
-    where
-        'v: 'a,
-    {
-        self.map_entry(field, value)
-    }
-
+    // Pair may be empty
     fn seq_begin(&mut self, len: Option<usize>) -> Result;
     fn seq_end(&mut self) -> Result;
 
+    // Pair *must* be contained by a single value
     fn seq_elem_begin(&mut self) -> Result;
     fn seq_elem_end(&mut self) -> Result;
-
-    fn seq_elem<'e, E: ValueRef<'e>>(&mut self, elem: E) -> Result
-    where
-        'e: 'a,
-    {
-        self.seq_elem_begin()?;
-        elem.stream(self)?;
-        self.seq_elem_end()
-    }
 
     fn for_all(&mut self) -> ForAll<&mut Self> {
         ForAll(self)
