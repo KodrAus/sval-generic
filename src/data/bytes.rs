8519863c6df014d95e969aed0534b0aaba6af518
Ashley Mannix <kodraus@hey.com>
2021-12-23 21:18:09 +1000

    use base64 as unstructured format for bytes

--- src/data/bytes.rs
+++ src/data/bytes.rs
@@ -3,6 +3,7 @@ use crate::{
     std::{
         fmt,
         ops::{Deref, DerefMut},
+        str,
     },
     Receiver, Result, Value,
 };
@@ -25,12 +26,59 @@ impl Bytes {
 
 impl fmt::Display for Bytes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // TODO: Change this to base64
-        for b in &self.0 {
-            write!(f, "{:X}", b)?;
-        }
+        // Unstructured encoding for bytes is base64
+        base64(&self.0, f)
+    }
+}
 
-        Ok(())
+fn base64(src: &[u8], mut w: impl fmt::Write) -> fmt::Result {
+    fn encode(src: &[u8; 3]) -> [u8; 4] {
+        // The base64 alphabet that inputs are encoded into
+        // Each group of 3 arbitrary bytes is encoded as 4 ASCII bytes
+        const BASE_64: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        const MASK: u32 = 0b00000000_00000000_00000000_00111111;
+
+        let incoming_group = ((src[0] as u32) << 16) | ((src[1] as u32) << 8) | (src[2] as u32);
+
+        [
+            BASE_64[((incoming_group >> 18) & MASK) as usize],
+            BASE_64[((incoming_group >> 12) & MASK) as usize],
+            BASE_64[((incoming_group >> 6) & MASK) as usize],
+            BASE_64[(incoming_group & MASK) as usize],
+        ]
+    }
+
+    // Work through each group of 3 bytes in the input together
+    let mut i = 3;
+    while i <= src.len() {
+        w.write_str(
+            str::from_utf8(&encode(
+                &src[i - 3..i].try_into().expect("infallible conversion"),
+            ))
+            .expect("invalid UTF8"),
+        )?;
+
+        i += 3;
+    }
+
+    // Deal with the remaining bytes
+    // These are null-padded to the right length and then encoded
+    match i - src.len() {
+        1 => {
+            let mut encoded = encode(&[src[i - 3], src[i - 2], b'\0']);
+            encoded[3] = b'=';
+
+            w.write_str(str::from_utf8(&encoded).expect("invalid UTF8"))
+        }
+        2 => {
+            let mut encoded = encode(&[src[i - 3], b'\0', b'\0']);
+            encoded[2] = b'=';
+            encoded[3] = b'=';
+
+            w.write_str(str::from_utf8(&encoded).expect("invalid UTF8"))
+        }
+        0 | 3 => Ok(()),
+        _ => unreachable!(),
     }
 }
 
@@ -93,5 +141,41 @@ impl<'a> ValueSource<'a, Bytes> for &'a str {
     #[inline]
     fn take_ref(&mut self) -> Result<&'a Bytes, source::TakeRefError<&Bytes, Self::Error>> {
         Ok(bytes(*self))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn base64_encode() {
+        for &(input, expected) in &[
+            (
+                "",
+                ""
+            ),
+            (
+                "7",
+                "Nw==",
+            ),
+            (
+                "ab",
+                "YWI=",
+            ),
+            (
+                "8bj",
+                "OGJq",
+            ),
+            (
+                "hliuvrahuloisegrm8opig y98pw45yt9849pmg s8y v xjn zd;lfgseo;,",
+                "aGxpdXZyYWh1bG9pc2Vncm04b3BpZyB5OThwdzQ1eXQ5ODQ5cG1nIHM4eSB2IHhqbiB6ZDtsZmdzZW87LA==",
+            ),
+        ] {
+            let mut actual = String::new();
+            base64(input.as_bytes(), &mut actual).expect("failed to encode");
+
+            assert_eq!(expected, &*actual);
+        }
     }
 }
