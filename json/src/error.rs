b5b19307bd2f82cb4674e428589ed6a306fdf7ad
KodrAus <kodraus@hey.com>
2023-02-01 08:11:59 +1000

    implement Error for json::Error

--- json/src/error.rs
+++ json/src/error.rs
@@ -14,6 +14,18 @@ pub(crate) enum ErrorKind {
     InvalidKey,
 }
 
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.kind {
+            ErrorKind::Generic => write!(f, "an error occurred serializing a value to JSON"),
+            ErrorKind::Fmt(_) => write!(f, "failed to write JSON into a formatter"),
+            #[cfg(feature = "std")]
+            ErrorKind::IO(_) => write!(f, "failed to write JSON"),
+            ErrorKind::InvalidKey => write!(f, "attempt to serialize a non-string key"),
+        }
+    }
+}
+
 impl Error {
     pub(crate) fn generic() -> Self {
         Error {
@@ -30,6 +42,23 @@ impl Error {
     pub(crate) fn invalid_key() -> Self {
         Error {
             kind: ErrorKind::InvalidKey,
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+mod std_support {
+    use super::*;
+
+    use std::error;
+
+    impl error::Error for Error {
+        fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+            match self.kind {
+                ErrorKind::Fmt(ref err) => Some(err),
+                ErrorKind::IO(ref err) => Some(err),
+                _ => None,
+            }
         }
     }
 }
